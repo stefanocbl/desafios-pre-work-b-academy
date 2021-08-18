2fef8331d5a32f140cb75a3ca8120211dfc6b3f2
stefanocbl <stefanocblima@gmail.com>
2021-08-17 21:25:46 -0300

    Adição do link para visualizar conteudo

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,3 +4,19 @@ document.querySelector('.app').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+const myLink = document.getElementById('link');
+const myApp = document.querySelector('.app');
+
+myLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (myApp.innerHTML == '') {
+    myApp.innerHTML = `
+    <h1>B. Academy</h1>
+    <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
+  `
+  }
+  else {
+    myApp.innerHTML = ''
+  }
+  console.log('clicou');
+})
