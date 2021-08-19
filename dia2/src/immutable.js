298e9f8c21680388ef39f0748d9ed9906bf0d867
stefanocbl <stefanocblima@gmail.com>
2021-08-18 22:26:33 -0300

    Adicionado dia2 e solução do 1ª desafio

---
+++ dia2/src/immutable.js
@@ -0,0 +1,15 @@
+const john = {
+  name: 'John',
+  surname: 'Doe',
+  age: 30,
+  hobbies: ['Surf', 'Design'],
+}
+
+const jane = {
+  ...john,
+  name: 'Jane',
+  hobbies: ['MuayThai', 'Programming']
+}
+
+console.log('John:', john)
+console.log('Jane:', jane)
