d3ff80b117ea419da4cc7e9ef4507c1202577131
stefanocbl <stefanocblima@gmail.com>
2021-08-20 18:50:58 -0300

    resolvido desafio nª3 item 2 com ajuda do fdaciuk

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -19,4 +19,41 @@ function fixCase (word) {
   return `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`
 }
 
-// correção pelo fdaciuk.
+// correção pelo fdaciuk. ^^^
+
+function $ (selector) {
+  return document.querySelector(selector)
+}
+
+const form = document.querySelector('[data-js="form"]')
+const select = document.createElement('select')
+const colors = ['#6B7280','#EF4444', '#0000FF', '#00FFFF', '#7FFF00']
+const colorsContainer = document.createElement('div')
+colorsContainer.style.display = 'flex'
+
+colors.forEach(color => {
+  const option = document.createElement('option')
+  option.value = color
+  option.textContent = color
+  select.appendChild(option)
+})
+
+
+select.addEventListener('change', (e) => {
+  colorsContainer.innerHTML = ''
+
+  Array.from(e.target.selectedOptions).forEach(option => {
+    const div = document.createElement('div')
+    div.style.width = '100px'
+    div.style.height = '100px'
+    div.style.background = option.value
+    document.body.appendChild(div)
+  })
+})
+
+select.setAttribute('multiple','')
+form.appendChild(select)
+document.body.appendChild(colorsContainer)
+
+// 00:54
+
